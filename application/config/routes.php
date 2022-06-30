cfe6ab2219c9c328505a9303c491a62f60331bd7
bragaz_rexita <bragazrexi@gmail.com>
2022-06-30 15:54:43 +0700

    Add route

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['dashboard']='Dashboard';
+$route['welcome']='Welcome';
+$route['pelanggan']='master/Pelanggan';
